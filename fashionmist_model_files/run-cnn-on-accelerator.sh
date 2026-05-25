66d8ef3c292cc7d8ef140c036082704efba7225a
Iain Hunter <iain.hunter@r3.group>
2026-05-25 18:40:50 +0100

    run both CPU and DSP inferences

--- fashionmist_model_files/run-cnn-on-accelerator.sh
+++ fashionmist_model_files/run-cnn-on-accelerator.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # runtime execution of model on beagley-ai
 
-export TIDL_RT_DEBUG=1
-export TIDL_RT_PERFSTATS=1
-export VX_ZONE_MASK=0xffffffff
-export ONNXRT_LOG_SEVERITY_LEVEL=0
+# some run time debug options
+#export TIDL_RT_DEBUG=1
+#export TIDL_RT_PERFSTATS=1
+#export VX_ZONE_MASK=0xffffffff
+#export ONNXRT_LOG_SEVERITY_LEVEL=0
 
+
+# run CPU execution
+python3 onnxrt_ep_fashionmist.py -d -m cl-ort-fashioncnn28
+# then DSP
 python3 onnxrt_ep_fashionmist.py -m cl-ort-fashioncnn28
